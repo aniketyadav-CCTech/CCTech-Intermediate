275682e81411ff3fe85438131337f4f13226e9a7
Aniket Yadav <aniket.yadav@cctech.co.in>
2022-12-12 19:27:32 +0530

    Submit Module Assignment

---
+++ ModulesAssignment/sort_by_length.cpp
@@ -0,0 +1,22 @@
+// The main program that prints all lines read from stdin sorted by their length.
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "sort_string_by_length.h"
+
+int main(int argc, char* argv[])
+{
+    std::string line;
+    std::vector<std::string> lines;
+
+    while(std::getline(std::cin,line))  // input lines to vector
+        lines.push_back(line);
+    
+    lines = sort_string_by_length(lines);   // sort by length
+
+    for(auto line:lines)    // print result
+    std::cout<<line<<std::endl;
+    
+    return EXIT_SUCCESS;
+}
