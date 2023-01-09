c798c8881477902748ac34ad08b653f9d76dddba
aniketyadav-CCTech <aniket.yadav@cctech.co.in>
2023-01-09 18:26:17 +0530

    Add support for SGD

--- CurrencyConvertor/Testing.cpp
+++ CurrencyConvertor/Testing.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
     CurrencyIO io("conversion.txt");
 
     // for list of currencies    
-    assert(io.currencyList == (vector<string>{"AUD","CHF","CNY","EUR","GBP","INR","JPY","USD"}));
+    assert(io.currencyList == (vector<string>{"AUD","CHF","CNY","EUR","GBP","INR","JPY","SGD","USD"}));
 
     // INR to USD conversion
     assert(io.convertCurrency("INR","USD",10)==0.121);
